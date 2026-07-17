dd71688b562403d4e948a8d919aa4a18c2ebf478
Farris Attiya <farrisattiya@Farriss-MacBook-Air.local>
2026-07-16 21:05:06 -0400

    updated the gitignore

--- server.js
+++ server.js
@@ -1,17 +1,38 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
+
+function authenticateToken(req,res,next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).json({ error: 'Token is required' });
+    }
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).json({ error: 'Invalid token' });
+        }
+        req.user = user;
+        next();
+    });
+}
+
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
+    console.log(`${req.method} ${req.url}`)
     next();
 });
 
 let users = [];
 let nextID = 1;
 
-app.get('/', (req, res) => res.send('MockMarketplace API AS WE SPEAK'));
+// Basic route to check if the server is running
+app.get('/', (req, res) => res.send('Welcome to MockMarketplace'));
 
+// ====== CRUD operations for users ====
 app.post('/users', (req, res) => {
     const {name, email} = req.body;
     if (!name || !email) {
@@ -26,18 +47,21 @@ app.get('/users', (req, res) => {
     res.json(users);
 });
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id',authenticateToken, (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
     if (!user) {
         return res.status(404).json({ error: 'User not found' });
     }
-    res.json(user);
+    res.json({id: user.id, name: user.name, email: user.email});
 });
 
-app.put('/users/:id', (req, res) => {
+app.put('/users/:id', authenticateToken, (req, res) => {
     const user = users.find(u => u.id === parseInt(req.params.id));
     if (!user) {
         return res.status(404).json({ error: 'User not found' });
+    }
+    if (!req.user || req.user.id !== parseInt(req.params.id)) {
+        return res.status(403).json({ error: 'You can only update your own account' });
     }
     const {name, email} = req.body;
     if (!name || !email) {
@@ -48,14 +72,54 @@ app.put('/users/:id', (req, res) => {
     res.json(user);
 });
 
-app.delete('/users/:id', (req, res) => {
+app.delete('/users/:id', authenticateToken, (req, res) => {
     const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
     if (userIndex === -1) {
         return res.status(404).json({ error: 'User not found' });
     }
+     if (!req.user || req.user.id !== parseInt(req.params.id)) {
+        return res.status(403).json({ error: 'You can only delete your own account' });
+    }
     users.splice(userIndex, 1);
     res.status(204).send();
 });
+
+// ====== Auth routes ======
+
+app.post('/signup', async (req,res) => {
+    const {name, email, password} = req.body;
+    if (!name || !email || !password) {
+        return res.status(400).json({ error: 'Name, email, and password are required' });
+    }
+    
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = {id: nextID++, name, email, password: hashedPassword};
+    users.push(newUser);
+    res.status(201).json({id: newUser.id, name: newUser.name, email: newUser.email});
+
+})
+
+app.post('/login', async (req,res) => {
+    const {email, password} = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and password are required' });
+    }
+    // Check and find the user by email
+    const user = users.find(p => p.email === email)
+    if (!user) {
+        return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+        return res.status(401).json({ error: 'Invalid email or password' });
+    }
+    const payload = {id: user.id, email: user.email};
+    const token = jwt.sign( payload, process.env.SECRET_KEY, {expiresIn: '1h'} );
+
+
+    res.json({ message: 'Login successful', token });
+});
+
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Route Not Found' });
